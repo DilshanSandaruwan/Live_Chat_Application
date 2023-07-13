393798fc2f8aa11db589cd2db3cdc83b7ee129cc
DilshanSandaruwan <sadaruwandilshan627@gmail.com>
2023-07-13 20:36:33 +0530

    inicialize logingController

--- src/main/java/controller/LogingController.java
+++ src/main/java/controller/LogingController.java
@@ -1,5 +1,28 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LogingController {
 
+    @FXML
+    private TextField txtname;
+    static String username;
+
+    @FXML
+    void NameOnAction(ActionEvent event) throws IOException {
+        username=txtname.getText();
+        txtname.clear();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(LogingController.class.getResource("/viwe/ClientForm.fxml"))));
+        stage.close();
+        stage.centerOnScreen();
+        stage.show();
+    }
 }
